4a6cd22c50b16275f48a4db69d6e400e51efb629
user137472995 <960318714161415-user137472995@noreply.coze.cn>
2026-05-21 10:26:46 +0800

    style: 性别选项添加padding和font-size样式
    Coze-Commit-Type: user
    Coze-User-ID: 960318714161415
    Coze-Conversation-ID: 4265135

--- src/App.vue
+++ src/App.vue
@@ -1213,7 +1213,7 @@ function askQuestion(question: string) {
 
         <div class="form-item">
           <label>性别 *</label>
-          <van-radio-group v-model="profileForm.gender" direction="horizontal" size="small">
+          <van-radio-group v-model="profileForm.gender" direction="horizontal" size="small" style="padding: 10px 15px;">
             <van-radio name="男">男</van-radio>
             <van-radio name="女">女</van-radio>
           </van-radio-group>
@@ -1686,6 +1686,10 @@ function askQuestion(question: string) {
 }
 .profile-form .form-item .van-radio-group {
   flex: 1;
+}
+.profile-form .form-item .van-radio__icon,
+.profile-form .form-item .van-radio__label {
+  font-size: 14px;
 }
 .record-form .form-header {
   margin-bottom: 12px;
